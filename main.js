7021b9ad181b178fae0169cd4e7ef2a1a7d2961a
AndreiTimofte96 <andrei.timofte96@gmail.com>
2017-05-11 16:53:27 +0300

    Codul nu e gata

--- main.js
+++ main.js
@@ -1,52 +1,63 @@
 (() => {
-  'use strict';
-  const request = require('request');
-  const rp = require('request-promise');
-  const cheerio = require('cheerio');
-  const Boilerpipe = require('boilerpipe');
+	'use strict';
+	const request = require('request');
+	const promise_title = require('request-promise');
+  //const promise_paragraph = require('request-promise');
+	const cheerio = require('cheerio');
+	const Boilerpipe = require('boilerpipe');
 
-  const url1 = "http://stiri.co/bayern-incerca-sa-transfere-starurile-lui-monaco-aceasta-vara/";
-  const url2 = "http://students.info.uaic.ro/~teodor.timofte/GameJs/index.html";
-  const url3 = "http://ortodoxia.rol.ro/cutremurtor-vezi-aici-ce-se-intampla-cu-sufletele-pruncilor-avortati--969637.html";
-  const url4 = "http://sanatatea.ro/7-semne-care-va-arata-ca-nu-sunteti-pentru-prima-data-pe-pamant/";
-  const url5 = "http://stirionline19.pw/daniela-crudu-tot-mai-desfigurata-de-operatiile-estetice/";
-  const url6 = "http://www.livebiz.ro/video/aude-pentru-prima-data-vocea-mamei-sale/";
-  
-  var options = {
+	const url1 = "http://stiri.co/bayern-incerca-sa-transfere-starurile-lui-monaco-aceasta-vara/";
+	const url2 = "http://gandul.md/monica-bellucci-surprinde-sexul-frecvent-si-legumele-proaspete-cheia-unei-vieti-sanatoase/"
+	const url3 = "http://ortodoxia.rol.ro/cutremurtor-vezi-aici-ce-se-intampla-cu-sufletele-pruncilor-avortati--969637.html";
+	const url4 = "http://sanatatea.ro/7-semne-care-va-arata-ca-nu-sunteti-pentru-prima-data-pe-pamant/";
+	const url5 = "http://stirionline19.pw/daniela-crudu-tot-mai-desfigurata-de-operatiile-estetice/";
+	const url6 = "http://www.livebiz.ro/video/aude-pentru-prima-data-vocea-mamei-sale/";
+	const url7 = "https://www.cocoon.ro/soc-carne-de-om-descoperita-la-mcdonalds/";
 
-    uri: url6,
-    transform: (body) => {
-      return cheerio.load(body);
-    }
-  };
-  
-  var boilerpipe = new Boilerpipe({
-    extractor: Boilerpipe.Extractor.ArticleSentences
-  }).setUrl(options.uri);
+	var options_title = {
+
+		uri: url7,
+		transform: (body) => {
+			return cheerio.load(body);
+		}
+	};
+
+	var boilerpipe = new Boilerpipe({
+    
+		extractor: Boilerpipe.Extractor.ArticleSentences,
+	}).setUrl(options_title.uri);
 
 
-  rp(options).then(($) => {
-      
+	console.log("<!DOCTYPE html>");
+	console.log("<html>");
+	console.log("<div>");
 
-      console.log("<!DOCTYPE html>");
-      console.log("<html>");
-      console.log("<div>");
-      
-      console.log("<h1>");
-      console.log($("title").text());
-      console.log("</h1>");
-      
-      boilerpipe.getText(function(err, text){
-        if (!err) {
-          console.log("<p>");
-          console.log(text);
-          console.log("</p>");
-          console.log("</div>");
-          console.log("<html>");
-        }
-      });
+	promise_title(options_title).then(($) => {
 
-    }).catch((e) => {
-      console.log(e);
+		console.log("<h1>");
+		console.log($("title").text());
+		console.log("</h1>");
+
+	}).catch((error) => {
+		  console.log(error);
+		});
+
+
+  var promise_paragraph = new Promise( () => { 
+
+    boilerpipe.getText(function(err, text){
+    
+      if (!err) {
+        console.log("<p>");
+        console.log(text);
+        console.log("</p>");      
+      }
+      else{
+        console.log(err);
+      }
+    });
+  }).then(() => {  
+    console.log("</div>");
+    console.log("</html>");
     });
 })();
